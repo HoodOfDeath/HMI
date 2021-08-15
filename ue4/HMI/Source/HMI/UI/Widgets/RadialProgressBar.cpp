9a34c0950c0249ba4f34c88e30fa462a226e5c18
HoodOfDeathST <k.namavir@gmail.com>
2021-08-15 16:23:38 +0300

    Added RadialProgressBar class, that based on RadialProgressBar material.

---
+++ ue4/HMI/Source/HMI/UI/Widgets/RadialProgressBar.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RadialProgressBar.h"
+#include "Components/Image.h"
+
+URadialProgressBar::URadialProgressBar(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+}
+
+void URadialProgressBar::NativeTick(const FGeometry& MovieSceneBlends, float InDeltaTime)
+{
+	Super::NativeTick(MovieSceneBlends, InDeltaTime);
+
+	static float Time = 0;
+	Time+=(InDeltaTime / 5);
+
+	Percent = FMath::Abs(FMath::Sin(Time));
+	
+	if (MaterialInstanceDynamic.IsValid())
+	{
+		MaterialInstanceDynamic->SetScalarParameterValue("Percent", Percent);
+	}
+}
+
+void URadialProgressBar::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (IsValid(Image_Base))
+	{
+		if (MaterialInstance)
+		{
+			MaterialInstanceDynamic = UMaterialInstanceDynamic::Create(MaterialInstance, this);
+			MaterialInstanceDynamic->SetScalarParameterValue("Percent", Percent);
+
+			Image_Base->SetBrushFromMaterial(MaterialInstanceDynamic.Get());
+		}
+	}
+}
---
+++ ue4/HMI/Source/HMI/UI/Widgets/RadialProgressBar.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "RadialProgressBar.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract)
+class HMI_API URadialProgressBar : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	URadialProgressBar(const FObjectInitializer& ObjectInitializer);
+
+	UPROPERTY(meta = ( BindWidget ))
+	class UImage* Image_Base;
+
+	UPROPERTY(EditAnywhere, Category="Progress bar")
+	UMaterialInstance* MaterialInstance;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Progress bar", meta=(UIMin = 0.0f, ClampMin = 0.0f, UIMax = 1.0f, ClampMax = 1.0f))
+	float Percent;
+
+private:
+	TWeakObjectPtr<UMaterialInstanceDynamic> MaterialInstanceDynamic;
+
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+
+	virtual void NativeConstruct() override;
+};

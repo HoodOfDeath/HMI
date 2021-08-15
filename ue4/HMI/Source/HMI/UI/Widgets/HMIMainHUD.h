a0720adf0e622314907a1abacb67a7fefb2a87d0
HoodOfDeathST <k.namavir@gmail.com>
2021-08-15 15:12:11 +0300

    Created Main Widget, that will hold entire UI.

---
+++ ue4/HMI/Source/HMI/UI/Widgets/HMIMainHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "HMIMainHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HMI_API UHMIMainHUD : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
